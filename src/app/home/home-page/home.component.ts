d4c6cfa23b583adaac5bfaf1f3a61ba46d82f332
fazalrahim <53045634+fazalrahim@users.noreply.github.com>
2024-03-12 14:39:48 +0400

    home basic design

--- src/app/home/home-page/home.component.ts
+++ src/app/home/home-page/home.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  heading: string = 'Home';
+  subdetails: string = 'Home Description';
+  tabularIcons: boolean = true;
+
+  infoObj: any = [
+    {
+      icon: 'shopping_cart',
+      title: '345',
+      subtitle: 'Total Orders'
+    },
+    {
+      icon: 'wallet',
+      title: '345',
+      subtitle: 'Total Expense'
+    }
+  ]
 }
